819246e854fc65b26c75881114d5c5755a0f6784
Jonathan Castro dos Santos <jonathandossantos.c@gmail.com>
2018-07-30 17:48:08 -0300

    feat: Create header and menu in mobile

---
+++ assets/src/js/menu.js
@@ -0,0 +1,6 @@
+$('body').css('background','black');
+/*
+$('').click(function() {
+    $('.nav-menu').fadein(slow);
+})
+*/
--- assets/src/js/text.js
+++
@@ -1,5 +0,0 @@
-var app = (function (document) {
-	document.getElementById('logo').innerHTML = 'Gulp';
-	document.getElementById('logoDesc').innerHTML = 'Automate and enhance your workflow';
-	document.getElementById('video').innerHTML = 'Hello World';
-})(document);
