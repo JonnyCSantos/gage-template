e9431898ef2bf05eeea0868b3db3062b8c24b923
Jonathan santos <jonathandossantos.c@gmail.com>
2018-07-30 21:45:35 -0300

    Feat: Menu.js works.

--- assets/src/js/menu.js
+++ assets/src/js/menu.js
@@ -1,6 +1,9 @@
-$('body').css('background','black');
-/*
-$('').click(function() {
-    $('.nav-menu').fadein(slow);
+$('#hamburguer-menu').click(function() {
+    $('.nav-menu').css('left','0');
+    $('.mask').css('position','fixed');
 })
-*/
+
+$('#hamburguer-x').click(function() {
+    $('.nav-menu').css('left','calc(-70% - 200px)');
+    $('.mask').css('position','initial');
+})
