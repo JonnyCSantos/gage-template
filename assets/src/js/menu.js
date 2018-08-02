b01771f6501fe0e28b6afa2d415ec4fdb22c25c3
Jonathan Castro dos Santos <jonathandossantos.c@gmail.com>
2018-08-02 07:34:01 -0300

    refactor: Refactoring menu.js and Styles

--- assets/src/js/menu.js
+++ assets/src/js/menu.js
@@ -1,9 +1,31 @@
+// Slide Menu
+
 $('#hamburguer-menu').click(function() {
     $('.nav-menu').css('left','0');
     $('.mask').css('position','fixed');
-})
+});
 
 $('#hamburguer-x').click(function() {
     $('.nav-menu').css('left','calc(-70% - 200px)');
     $('.mask').css('position','initial');
-})
+});
+
+$('.mask').click(function() {
+    $('.nav-menu').css('left','calc(-70% - 200px)');
+    $('.mask').css('position','initial');
+});
+
+// Points Balloon
+
+$('html').click(function() {
+    $('.gup-details').hide(); 
+ });
+ 
+ $('.status').click(function(event){
+      event.stopPropagation();
+ });
+ 
+ $('#gup-points').click(function(event){
+      $('.gup-details').toggle('fast');
+ });
+ 
